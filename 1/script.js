e5788e33fb7d773015c7b69930e1baf581c99c9f
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2023-10-09 12:43:30 +0700

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,27 @@
+function checkloveper(){
+
+    var name = document.getElementById('name').value;
+    var lname = document.getElementById('lname').value;
+  
+    if(name == ""){
+     alert('Введите ваше имя');
+    }else if (name.length <=2 ) {
+     alert('Мин длина 3')
+    }else if(!isNaN(name)){
+     alert('Никаких чисел');
+    }
+  
+    else if(lname == ""){
+     alert('Введите имя вашей второй половинки');
+    }else if (lname.length <=2 ) {
+     alert('Мин длина 3')
+    }else if(!isNaN(lname)){
+     alert('Никаких чисел');
+    }
+    else{
+    var lovdata = Math.random() * 100;
+    lovdata = Math.floor(lovdata);
+    //document.getElementById('lovevalue').value = lovdata + "%";
+    alert('{name} подходит к {lname} на {lovdata} процентов!');
+    }
+   }
