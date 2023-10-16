2109aef3e806e3678a3e880c7bc3e4ef7aceb6da
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2023-10-16 11:46:55 +0700

    Update script.js

--- 2/script.js
+++ 2/script.js
@@ -1,32 +1,23 @@
 window.onload = () => {
     let button = document.querySelector("#btn");
- 
     button.addEventListener("click", calculateBMI);
 };
- 
 function calculateBMI() {
- 
     let height = parseInt(document.querySelector("#height").value);
- 
     let weight = parseInt(document.querySelector("#weight").value);
- 
     let result = document.querySelector("#result");
- 
-    if (height === "" || isNaN(height)) result.innerHTML = "Введите рост!";
- 
-    else if (weight === "" || isNaN(weight)) result.innerHTML = "Введите вес!";
- 
+    if (height === "" || isNaN(height)) 
+        result.innerHTML = "Введите рост!";
+    else if (weight === "" || isNaN(weight)) 
+        result.innerHTML = "Введите вес!";
     else {
- 
         let bmi = (weight / (height * height)).toFixed(2);
- 
-        if (bmi <= 18.5) result.innerHTML = `Недостаточный вес : <span>${bmi}</span>`;
- 
+        if (bmi <= 18.5) 
+            result.innerHTML = `Недостаточный вес : <span>${bmi}</span>`;
         else if (bmi >= 18.5 && bmi <= 25) 
             result.innerHTML = `Нормально : <span>${bmi}</span>`;
         else if (bmi >= 25 && bmi <= 30) 
             result.innerHTML = `У вас излишек веса : <span>${bmi}</span>`;
- 
         else result.innerHTML = `Ожирение : <span>${bmi}</span>`;
     }
 }
