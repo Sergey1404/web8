01b57a61cfb339c73dc91c2916de4936ba03ac8d
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2023-10-16 12:23:11 +0700

    Update script.js

--- 4/script.js
+++ 4/script.js
@@ -1,3 +1,3 @@
 let friend = ['Дима', 'Катя', 'Петр', 'Лена'];
-var rand = Math.floor(Math.random() * arr.length);
+var rand = Math.floor(Math.random() * friend.length);
 alert('Оплачивать будет ' + (friend[rand]));
